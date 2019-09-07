55852967768c1b5a48bf5d242fe8c35169d4874e
jaychaturvedi <jaychaturvedi18@gmail.com>
2019-09-07 18:09:14 +0530

    Added static files

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -109,8 +109,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'producthunt/static/')
+]
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
