ca18ab401db5f4f7b96bda21d7aeafe613c3b6d6
jaychaturvedi <jaychaturvedi18@gmail.com>
2019-09-07 23:43:17 +0530

    Created dbmodels for product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length = 250)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to = 'images/')
+    icons = models.ImageField(upload_to = 'images/')
+    votes_total = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:200]+'...'
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime("%d %b %Y")
