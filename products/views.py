8814bacc976dbb62f3d8f8e4c0563ceda9db54f5
jaychaturvedi <jaychaturvedi18@gmail.com>
2019-09-09 00:45:22 +0530

    finished site

--- products/views.py
+++ products/views.py
@@ -5,10 +5,12 @@ from django.utils import timezone
 
 
 def home(request):
-    return render(request, 'products/home.html')
+    products = Product.objects
+
+    return render(request, 'products/home.html', {'products': products})
 
 
-@login_required
+@login_required(login_url="/accounts/signup")
 def create(request):
     if request.method == "POST":
         if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
@@ -31,7 +33,15 @@ def create(request):
             return render(request, 'products/create.html', {'error': 'All fields are mandatory'})
     else:
         return render(request, 'products/create.html')
-@login_required
+
 def detail(request, product_id):
     product = get_object_or_404(Product, pk= product_id)
     return render(request, 'products/detail.html',{'product':product})
+
+@login_required(login_url="/accounts/signup")
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return redirect('/products/' + str(product.id))
